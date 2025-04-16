7997862511d5c0d74cba873f272ae7d84a0ca8ed
Daniel Simko <simko.daniel@hotmail.com>
2025-04-16 12:58:08 +0200

    implement deposit test

--- test/SimpleBank.t.sol
+++ test/SimpleBank.t.sol
@@ -6,6 +6,7 @@ import {SimpleBank} from "../src/SimpleBank.sol";
 
 contract SimpleBankTest is Test {
     SimpleBank public bank;
+    address public user = makeAddr("user");
 
     function setUp() public {
         bank = new SimpleBank();
@@ -19,10 +20,23 @@ contract SimpleBankTest is Test {
     }
 
     function test_Deposit() public {
+        uint256 depositAmount = 1 ether;
+        vm.deal(user, depositAmount); // Give the user some ETH
+        uint256 before = user.balance;
+        // Makes all subsequent transactions in the test (until vm.stopPrank() is called)
+        // behave as if they were sent by the user address, i.e., msg.sender and tx.origin are set to user.
+        vm.startPrank(user);
+        bank.deposit{value: depositAmount}();
+        assertEq(bank.getBalance(), depositAmount); //needs to run here so that msg.sender is the user
+        vm.stopPrank();
+
+        assertEq(user.balance, before - depositAmount);
+        assertEq(address(bank).balance, depositAmount);
     }
 
     function test_Withdraw() public {
-    } 
+        //selector for test_withdraw - deposit 1 eth for user1 and 2eth for user 2
+    }
 
     function testFuzz_Deposit(uint256 x) public {
     }
